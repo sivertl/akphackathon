a6cbf2c8c3e9e331767c724b430d65b4ee3f3310
sivert løken <sivertlo@stud.ntnu.no>
2019-03-21 23:51:14 +0100

    Added map view and hyperlinks

--- senior_track/track/urls.py
+++ senior_track/track/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'base'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('map', views.map, name = 'map')
 ]
--- senior_track/track/views.py
+++ senior_track/track/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'track/index.html')
+def map(request):
+    return render(request, 'map/map.html')
+    
