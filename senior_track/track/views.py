d187f40d1e60091e6f0d2b17bfdf5dc59737a5ef
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2019-03-21 22:03:49 +0100

    Works!

--- senior_track/track/views.py
+++ senior_track/track/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'templates/index.html')
+    return render(request, 'track/index.html')
