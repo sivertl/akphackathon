d968c35f8db7935e8ade3d21b96b8c5b34668b78
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2019-03-22 00:15:37 +0100

    Can now send get request to server

--- senior_track/track/urls.py
+++ senior_track/track/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'track'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('check_alerts/', views.check_alerts, name='check_alerts')
 ]
--- senior_track/track/views.py
+++ senior_track/track/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'track/index.html')
+
+def check_alerts(request):
+    return JsonResponse({"hey":"hey"})
