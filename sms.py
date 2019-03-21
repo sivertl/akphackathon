59fac3ab4812f51a68a4f73a9a4c482a3d47b6ce
osteinnes <omsteinn@stud.ntnu.no>
2019-03-21 22:24:22 +0100

    Adding sms.py for sending alarm to workers

---
+++ sms.py
@@ -0,0 +1,25 @@
+from twilio.rest import Client
+import json
+
+def alarm(message):
+
+    with open('sms_config.json') as json_data_file:
+        data = json.load(json_data_file)
+
+    account_sid = data['sms']['account_sid']
+    auth_token = data['sms']['auth_token']
+
+    client = Client(account_sid, auth_token)
+
+    host_number = data['sms']['host_number']
+    workers = data['sms']['workers']
+    
+    
+    for worker in workers:
+        client.messages.create(to=worker, 
+                            from_=host_number, 
+                            body=message)
+
+
+if __name__ == "__main__":
+    alarm("HJELP")
