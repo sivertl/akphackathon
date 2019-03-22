310f1a7b0e1d41820d5b995c8e09acd3940ce79c
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2019-03-22 07:25:11 +0100

    MERGE

--- senior_track/track/scripts/sms.py
+++ senior_track/track/scripts/sms.py
@@ -3,11 +3,7 @@ import json
 
 def alarm(message):
 
-<<<<<<< HEAD
     with open('./track/conf/sms_config.json') as json_data_file:
-=======
-    with open('C:/Users/siver/Desktop/Hackathon2019/akphackathon/senior_track/track/conf/sms_config.json') as json_data_file:
->>>>>>> a8c86dfc992123ad8e810e95e65f9c6d65d7328b
         data = json.load(json_data_file)
 
     account_sid = data['sms']['account_sid']
